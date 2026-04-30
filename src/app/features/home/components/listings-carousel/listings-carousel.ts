0304c03cd888707bd2104ecabde5e87401627a91
MakuOSV6PC-597\Oleh <opoiasnik@gmail.com>
2026-04-30 19:10:48 +0200

    fix: remove carousel

--- src/app/features/home/components/listings-carousel/listings-carousel.ts
+++ src/app/features/home/components/listings-carousel/listings-carousel.ts
@@ -1,13 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
-import { CarouselModule } from 'primeng/carousel';
 import { ListingResponse } from '../../../listings/models/listing.model';
 import { ListingCard } from '../listing-card/listing-card';
 
 @Component({
   selector: 'app-listings-carousel',
   standalone: true,
-  imports: [CommonModule, CarouselModule, ListingCard],
+  imports: [CommonModule, ListingCard],
   templateUrl: './listings-carousel.html',
   styleUrl: './listings-carousel.scss'
 })
@@ -15,11 +14,4 @@ export class ListingsCarousel {
   @Input({ required: true }) title!: string;
   @Input({ required: true }) items: ListingResponse[] = [];
   @Input() imageSeedOffset = 0;
-
-  readonly responsiveOptions = [
-    { breakpoint: '1400px', numVisible: 6, numScroll: 1 },
-    { breakpoint: '1200px', numVisible: 4, numScroll: 1 },
-    { breakpoint: '900px', numVisible: 3, numScroll: 1 },
-    { breakpoint: '640px', numVisible: 1, numScroll: 1 }
-  ];
 }
