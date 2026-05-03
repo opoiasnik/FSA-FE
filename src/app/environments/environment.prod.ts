1289ce3e8870b9c741b082fc1f139d0a6e23b762
MakuOSV6PC-597\Oleh <opoiasnik@gmail.com>
2026-05-03 14:24:33 +0200

    use rental keykloak realm

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -2,10 +2,10 @@ export const environment = {
   production: true,
   apiBaseUrl: '/api/listings',
   auth: {
-    issuer: 'https://poiasniko.20.107.204.3.nip.io/auth/realms/FSA',
+    issuer: 'https://poiasniko.20.107.204.3.nip.io/auth/realms/rental',
     redirectUri: 'https://poiasniko.20.107.204.3.nip.io/',
     postLogoutRedirectUri: 'https://poiasniko.20.107.204.3.nip.io/',
-    clientId: 'fsa-client',
+    clientId: 'rental-client',
     responseType: 'code',
     scope: 'openid profile email',
     showDebugInformation: false
